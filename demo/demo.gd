c53fa2fb1117f8462836613aaa9c21172b16a753
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-01-06 23:45:25 +0100

    Update the demo to make use of localized date formatting

--- demo/demo.gd
+++ demo/demo.gd
@@ -3,9 +3,21 @@
 
 extends Control
 
-const Date = preload("res://addons/godot_date/date.gd")
+const Date := preload("res://addons/godot_date/date.gd")
+
+onready var container := $VBoxContainer as Container
 
 func _ready():
 	var date := Date.new("2017-06-15T21:15:00Z")
-	date.year += 5
-	$Label.text = str(date.format())
+	add_label(date.format())
+	add_label(date.format("LT"))
+	add_label(date.format("LTS"))
+	add_label(date.format("L"))
+	add_label(date.format("LL"))
+	add_label(date.format("LLL"))
+	add_label(date.format("LLLL"))
+
+func add_label(text: String) -> void:
+	var label := Label.new()
+	label.text = text
+	container.add_child(label)
