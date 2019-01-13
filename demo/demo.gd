d23578488cd0c76b7fea2c79952ec975a8375f0d
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-01-13 23:57:49 +0100

    Demonstrate `is_before()` usage

--- demo/demo.gd
+++ demo/demo.gd
@@ -19,6 +19,11 @@ func _ready():
 	add_label(date.format("LLLL"))
 	add_label("Custom formats")
 	add_label(date.format("YYYY-MM"))
+	add_label("Before / After")
+	var compare_date := Date.new("2017-07-01T09:00:00Z")
+	add_label(str(date.is_before(compare_date, Date.Precision.YEAR)))
+	add_label(str(date.is_before(compare_date, Date.Precision.MONTH)))
+	add_label(str(date.is_before(compare_date, Date.Precision.DAY)))
 
 func add_label(text: String) -> void:
 	var label := Label.new()
